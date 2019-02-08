b2f56b71043e3b1608be9902d847fe70ae579729
Marcos Antonio Lacerda Pereira <marcos.lacerda@serpro.gov.br>
2019-02-08 11:38:58 -0300

    ajustes migracao

--- launch/build-local.sh
+++ launch/build-local.sh
@@ -4,7 +4,7 @@ backendHome=$1
 frontendHome=$2
 
 cd $frontendHome
-npm run build:spring-inside:prod
+npm run build:spring-inside
 mkdir -p $backendHome/src/main/resources/static
 rm -r $backendHome/src/main/resources/static/*
 cp -r dist/* $backendHome/src/main/resources/static/
