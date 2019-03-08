fa95aec2b8c92b2e39fda718a3e528138d540366
Marcos Lacerda <marcos.lacerda@serpro.gov.br>
2019-03-08 18:36:17 -0300

    wip

--- data-init/init.sh
+++ data-init/init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#psql -d pedagogia -U na -f 01-reset.sql
+psql -d pedagogia -U na -f 01-reset.sql
 psql -d pedagogia -U na -f 02-create.sql
-psql -d pedagogia -U na -f 04-basic.sql
-psql -d pedagogia -U na -f 20-alunos.sql
-psql -d pedagogia -U na -f 20-turmas.sql
-psql -d pedagogia -U na -f 30-alunos_turmas.sql
+psql -d pedagogia -U na -f 02-dados.sql
